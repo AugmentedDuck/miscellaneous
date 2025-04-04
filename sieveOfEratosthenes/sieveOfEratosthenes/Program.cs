167f02ed269d533091a2f56ff6b52f34b2e6d24a
AugmentedDuck <anders.m.matzen@gmail.com>
2025-04-04 13:32:17 +0200

    It now works as expected

--- sieveOfEratosthenes/sieveOfEratosthenes/Program.cs
+++ sieveOfEratosthenes/sieveOfEratosthenes/Program.cs
@@ -1,10 +1,68 @@
-﻿namespace sieveOfEratosthenes
+﻿
+namespace sieveOfEratosthenes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int CheckToNumber = ReadConsoleToInt("To what number should be tested?");
+
+            int[] numberLine = new int[CheckToNumber - 2];
+
+            for (int i = 0; i < numberLine.Length; i++)
+            {
+                numberLine[i] = i + 2;
+            }
+
+            Console.WriteLine("Running");
+
+            int[] primes = SieveOfEratosthenes(numberLine);
+
+            foreach (int prime in primes)
+            {
+                Console.WriteLine(prime);
+            }
+        }
+
+        private static int[] SieveOfEratosthenes(int[] numberLine)
+        {
+            foreach (int currentNumber in numberLine)
+            {
+                for(int i = 0; i < numberLine.Length; i++)
+                {
+                    if (numberLine[i] == currentNumber || numberLine[i] == 0 || currentNumber == 0) { continue; }
+
+                    if (numberLine[i] % currentNumber == 0)
+                    {
+                        numberLine[i] = 0;
+                    }
+                }
+            }
+
+            numberLine = numberLine.Where(x => x != 0).ToArray();
+
+            return numberLine;
+        }
+
+        private static int ReadConsoleToInt(string question)
+        {
+            Console.WriteLine(question);
+
+            while (true)
+            {
+                try
+                {
+                    string input = Console.ReadLine()!;
+                    
+                    int inputInt = int.Parse(input);
+
+                    return inputInt;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Enter a number");    
+                }
+            }
         }
     }
 }
