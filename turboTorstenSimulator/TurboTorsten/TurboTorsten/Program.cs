54f73094f75a98920492970de018739c13632eda
AugmentedDuck <anders.m.matzen@gmail.com>
2026-03-19 19:14:31 +0100

    Add TurboTorsten simulator project
    Create a new TurboTorsten solution and console project. Adds Program.cs which runs 1,000,000 simulations of a 6-shot-glass game, collecting rounds per simulation and printing average, median, min and max. Implements simulateGame using a boolean[6] and Random to toggle glasses until six consecutive hits; returns rounds. Project file targets net10.0 with implicit usings and nullable enabled. Note: Program contains some commented/unused variables (drinks, data array) that can be cleaned up later.

---
+++ turboTorstenSimulator/TurboTorsten/TurboTorsten/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace TurboTorsten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            List<int> rounds = new List<int>();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                rounds.Add(simulateGame());
+            }
+
+            int averageRound = 0;
+            //int averageDrinks = 0;
+
+            foreach (int round in rounds)
+            {
+                averageRound += round;
+                //averageDrinks += round[0];
+            }
+
+            averageRound /= rounds.Count;
+            //averageDrinks /= rounds.Count;
+
+            rounds.Sort();
+
+            int roundMean = rounds[rounds.Count / 2];
+
+            Console.WriteLine(averageRound.ToString() + " rounds\nThe mean number of rounds is: " + roundMean + "\nMin: " + rounds.Min() + "\nMax: " + rounds.Max());
+        }
+
+        static int simulateGame()
+        {
+            int rounds = 0;
+            int currentRound = 0;
+            int drinks = 0;
+
+
+            bool[] shotGlasses = { false, false, false, false, false, false };
+
+            Random rng = new();
+
+            while (currentRound != 6)
+            {
+                int dice = rng.Next(0, 6);
+
+                if (!shotGlasses[dice])
+                {
+                    shotGlasses[dice] = true;
+                    rounds++;
+                    currentRound = 0;
+                } else
+                {
+                    shotGlasses[dice] = false;
+                    currentRound++;
+                    drinks++;
+                }
+            }
+
+            int[] data = { drinks, rounds };
+
+            return rounds;
+        }
+    }
+}
