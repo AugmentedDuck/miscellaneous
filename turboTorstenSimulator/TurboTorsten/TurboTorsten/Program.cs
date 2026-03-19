3abe23ed5d45b69fcd894218e3432a4bc5ca62cd
AugmentedDuck <anders.m.matzen@gmail.com>
2026-03-19 21:30:46 +0100

    Parallelize simulations and optimize RNG/bitmask
    Increase simulations to 10M and parallelize the run using Parallel.For with thread-local histograms aggregated into a global histogram (protected by a lock). Replace shared Random with ThreadLocal<Random> seeded from Guid for thread safety. Replace the bool[] shotGlasses with an int bitmask for faster state updates and use rng.Value in simulateGame. Remove single-threaded sum/min/max tracking and instead write distribution and CDF outputs to CSV files; print a completion message when done.

--- turboTorstenSimulator/TurboTorsten/TurboTorsten/Program.cs
+++ turboTorstenSimulator/TurboTorsten/TurboTorsten/Program.cs
@@ -2,45 +2,63 @@
 {
     internal class Program
     {
-        static readonly Random rng = new();
+        static ThreadLocal<Random> rng = new(() => new Random(Guid.NewGuid().GetHashCode()));
 
         static void Main(string[] args)
         {
             Console.WriteLine("Running TurboTorsten Simulation");
 
-            const int numberOfSimulations = 1000000;
+            const int numberOfSimulations = 10000000;
+           
+            Dictionary<int, int> globalHistogram = new();
 
-            long sumRounds = 0;
-            int min = int.MaxValue;
-            int max = int.MinValue;
-            
-            Dictionary<int, int> roundsDistribution = new();
+            object lockObj = new();
 
-            for (int i = 0; i < numberOfSimulations; i++)
-            {
-                int rounds = simulateGame();
+            Parallel.For(0, numberOfSimulations,
+                () => new Dictionary<int, int>(),
 
-                sumRounds += rounds;
-
-                min = rounds < min ? rounds : min;
-                max = rounds > max ? rounds : max;
-
-                if (roundsDistribution.ContainsKey(rounds))
+                (i, state, localHist) =>
                 {
-                    roundsDistribution[rounds]++;
-                } 
-                else
+                    int rounds = simulateGame();
+
+                    if (localHist.ContainsKey(rounds))
+                    {
+                        localHist[rounds]++;
+                    }
+                    else
+                    {
+                        localHist[rounds] = 1;
+                    }
+
+                    return localHist;
+                },
+
+                localHist =>
                 {
-                    roundsDistribution[rounds] = 1;
+                    lock (lockObj)
+                    {
+                        foreach (var kvp in localHist)
+                        {
+                            if (globalHistogram.ContainsKey(kvp.Key))
+                            {
+                                globalHistogram[kvp.Key] += kvp.Value;
+                            }
+                            else
+                            {
+                                globalHistogram[kvp.Key] = kvp.Value;
+                            }
+                        }
+                    }
                 }
-            }
+            );
 
-            List<KeyValuePair<int, int>> sortedDistribution = roundsDistribution.OrderBy(kvp => kvp.Key).ToList();
+            var sortedHistogram = globalHistogram.OrderBy(kvp => kvp.Key).ToList();
 
             using (StreamWriter writer = new("rounds_distribution.csv"))
             {
                 writer.WriteLine("Rounds;Count;Probability");
-                foreach (var kvp in sortedDistribution)
+
+                foreach (var kvp in sortedHistogram)
                 {
                     double probability = (double)kvp.Value / numberOfSimulations;
                     writer.WriteLine($"{kvp.Key};{kvp.Value};{probability}");
@@ -51,24 +69,17 @@
             {
                 writer.WriteLine("Rounds;CDF");
 
-                double cumulativeProbability = 0.0;
+                double cumulativeProbability = 0;
 
-                foreach (var kvp in sortedDistribution)
+                foreach (var kvp in sortedHistogram)
                 {
                     double probability = (double)kvp.Value / numberOfSimulations;
                     cumulativeProbability += probability;
-
                     writer.WriteLine($"{kvp.Key};{cumulativeProbability}");
                 }
             }
 
-            double average = (double)sumRounds / numberOfSimulations;
-
-            Console.WriteLine(
-                $"{average} rounds\n" +
-                $"Min: {min}\n" +
-                $"Max: {max}"
-            );
+            Console.WriteLine("Simulation completed. Results saved to rounds_distribution.csv and CDF.csv");
         }
 
         static int simulateGame()
@@ -76,21 +87,21 @@
             int rounds = 0;
             int currentRound = 0;
 
-            bool[] shotGlasses = { false, false, false, false, false, false };
+            int shotMask = 0;
 
 
             while (currentRound != 6)
             {
-                int dice = rng.Next(0, 6);
+                int dice = rng.Value.Next(0, 6);
 
-                if (!shotGlasses[dice])
+                if ((shotMask & (1 << dice)) == 0)
                 {
-                    shotGlasses[dice] = true;
+                    shotMask |= (1 << dice);
                     rounds++;
                     currentRound = 0;
                 } else
                 {
-                    shotGlasses[dice] = false;
+                    shotMask &= ~(1 << dice);
                     currentRound++;
                 }
             }
