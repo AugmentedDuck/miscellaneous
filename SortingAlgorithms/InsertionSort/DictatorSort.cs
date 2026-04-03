3000770978fea4dfaaa60286449419c30e11096d
AugmentedDuck <anders.m.matzen@gmail.com>
2026-04-03 21:33:10 +0200

    Add DictatorSort stub
    Add new DictatorSort class implementing ISortAlgorithm under SortingAlgorithms/InsertionSort. Provides Name() returning "Dictator Sort" and a placeholder Sort() that currently returns the input array unchanged; serves as a stub for a future sorting algorithm implementation.

---
+++ SortingAlgorithms/InsertionSort/DictatorSort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    internal class DictatorSort : ISortAlgorithm
+    {
+        public string Name()
+        {
+            return "Dictator Sort";
+        }
+
+        public int[] Sort(int[] array)
+        {
+            return array;
+        }
+    }
+}
