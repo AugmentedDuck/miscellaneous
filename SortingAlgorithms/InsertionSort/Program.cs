8020a8311f5eecac965b0e8e70375821ef8b506f
AugmentedDuck <anders.m.matzen@gmail.com>
2026-04-04 00:21:40 +0200

    Add TestCorrectness and annotate menu items
    Add a TestCorrectness helper to validate a chosen sorting algorithm using a randomly generated 16-element array and compare against Array.Sort. Temporarily disable the bulk performance test loop and instead print the correctness result. Annotate several console menu entries with status tags ([V], [N/A]) to indicate validity or not applicable algorithms. Also add a System.Drawing using (likely unused) and minor whitespace/console output adjustments in SortingAlgorithms/InsertionSort/Program.cs.

--- SortingAlgorithms/InsertionSort/Program.cs
+++ SortingAlgorithms/InsertionSort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Drawing;
 
 namespace SortingAlgorithms
 {
@@ -26,8 +27,8 @@ namespace SortingAlgorithms
             //Console.WriteLine("(15) Library sort");
             //Console.WriteLine("(16) Shellsort");
             //Console.WriteLine("(17) Comb sort");
-            Console.WriteLine("(18) Insertion sort");
-            Console.WriteLine("(19) Bubble sort");
+            Console.WriteLine("(18) Insertion sort [V]");
+            Console.WriteLine("(19) Bubble sort [V]");
             //Console.WriteLine("(20) Cocktail Shaker sort");
             //Console.WriteLine("(21) Gnome sort");
             //Console.WriteLine("(22) Odd-even sort");
@@ -41,16 +42,16 @@ namespace SortingAlgorithms
             //Console.WriteLine("(30) Spreadsort");
             //Console.WriteLine("(31) Burstsort");
             //Console.WriteLine("(32) Flashsort");
-            Console.WriteLine("! (33) Bogosort !");
+            Console.WriteLine("! (33) Bogosort ! [V]");
             //Console.WriteLine("! (34) Bogo-Bogosort !");
             //Console.WriteLine("(35) Stooge sort");
             //Console.WriteLine("(36) Slowsort");
             //Console.WriteLine("(37) Thanos sort - Removes Data");
-            Console.WriteLine("(38) Stalin sort - Removes Data");
-            Console.WriteLine("! (39) Miracle sort !");
-            Console.WriteLine("! (40) Bozosort !");
-            Console.WriteLine("(41) Quantum sort - Requires the correct universe");
-            Console.WriteLine("(42) Dictator sort");
+            Console.WriteLine("(38) Stalin sort - Removes Data [V]");
+            Console.WriteLine("! (39) Miracle sort ! [N/A]");
+            Console.WriteLine("! (40) Bozosort ! [V]");
+            Console.WriteLine("(41) Quantum sort - Requires the correct universe [N/A]");
+            Console.WriteLine("(42) Dictator sort [N/A]");
             //Console.WriteLine("(43) Pancake sort");
             //Console.WriteLine("(44) Bitonic sort");
 
@@ -63,8 +64,13 @@ namespace SortingAlgorithms
             }
 
             ISortAlgorithm algorithm = ChooseAlgorithm(input);
-            Console.WriteLine($"{algorithm.Name()}\nSize of Array;Time (ms);Large Span numbers\n");
 
+            Console.WriteLine(TestCorrectness(algorithm));
+
+
+
+            /* Disabled for testing correctness of algorithms
+            Console.WriteLine($"{algorithm.Name()}\nSize of Array;Time (ms);Large Span numbers\n");
 
             int arraySize = 16;
 
@@ -72,6 +78,48 @@ namespace SortingAlgorithms
             {
                 Console.WriteLine(TestAlgorithm(algorithm, arraySize));
                 arraySize *= 2;
+            }
+            */
+        }
+
+        private static string TestCorrectness(ISortAlgorithm algorithm)
+        {
+            int[] array = new int[16];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(int.MinValue, int.MaxValue);
+            }
+
+            int[] expected = (int[])array.Clone();
+            Array.Sort(expected);
+
+            int[] actual = (int[])array.Clone();
+            algorithm.Sort(actual);
+            
+            if (actual.Length == expected.Length)
+            {
+                for (int i = 0; i < actual.Length; i++)
+                {
+                    if (actual[i] != expected[i])
+                    {
+                        return "Algorithm is incorrect, length IS equal but data IS NOT";
+                    }
+                }
+                return "Algorithm is Correct";
+            }
+            else
+            {
+                expected = (int[])actual.Clone();
+                Array.Sort(expected);
+                for (int i = 0; i < actual.Length; i++)
+                {
+                    if (actual[i] != expected[i])
+                    {
+                        return "Algorithm is incorrect, length AND data ARE NOT equal";
+                    }
+                }
+                return "Algorithm is incorrect, length IS NOT equal but data IS";
             }
         }
 
