fb2979dbadf716a43d70371c5551ea55e168d393
AugmentedDuck <anders.m.matzen@gmail.com>
2026-04-03 21:32:29 +0200

    Add InsertionSort implementation
    Introduce SortingAlgorithms/InsertionSort/InsertionSort.cs: an internal InsertionSort class that implements ISortAlgorithm. Implements Name() returning "Insertion Sort" and Sort(int[] array) which performs an in-place, stable insertion sort (O(n^2)) and returns the sorted array.

---
+++ SortingAlgorithms/InsertionSort/InsertionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    internal class InsertionSort : ISortAlgorithm
+    {
+        public string Name()
+        {
+            return "Insertion Sort";
+        }
+
+        public int[] Sort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+
+                array[j + 1] = key;
+            }
+
+
+            return array;
+        }
+    }
+}
